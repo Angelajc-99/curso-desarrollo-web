6035be6eb4f2d4e71d1566083958faaf06cbe85a
Angelajc-99 <nicolejara053@gmail.com>
2022-11-24 13:57:53 +0100

    clase del jueves 24 de noviembre

---
+++ frontend/JavaScript/ejercicios/botones de colores/script.js
@@ -0,0 +1,9 @@
+// Función sencilla
+function colorChange(color) {
+    document.body.style.background = color;
+    console.log('has pulsado un boton')
+}
+
+function colorReset() {
+    document.body.style.background = 'linear-gradient(to right, rgba(255, 0, 0, 0), rgb(113, 172, 221))'
+}
