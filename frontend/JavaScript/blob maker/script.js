478f16b911da29460544fac80826a148c31d1e5c
Angelajc-99 <nicolejara053@gmail.com>
2022-11-25 13:40:50 +0100

    clase del viernes 25 de noviembre

---
+++ frontend/JavaScript/blob maker/script.js
@@ -0,0 +1,51 @@
+// El código númerico del border-radius
+let outputcode = document.querySelector('outpur-code')
+// array con los sliders
+const sliders = document.querySelectorAll('input[type="range"]');
+
+// recorremos el array para asignar un evento a cada slider
+sliders.forEach(function (slider) {
+    slider.addEventListener('change', createBlob);
+});
+
+// array con los imput númericos
+const inputs = document.querySelectorAll('input[type="number"]');
+
+// asignamos eventos en los inputs
+inputs.forEach(function (input) {
+    input.addEventListener('change', createBlob);
+});
+// función que crea un blob nuevo cada vez que alteramos un slider o input
+function createBlob() {
+    let radiusOne = sliders[0].value;
+    let radiusTwo = sliders[1].value;
+    let radiusThree = sliders[2].value;
+    let radiusFour = sliders[3].value;
+
+    // recogemos los valore númericos de altura y anchura
+    let blobHeight = inputs[0].value;
+    let blobWidth = inputs[1].value;
+
+
+    // Creamos una variable que reúna todos estos valores 
+    let borderRadius = `${radiusOne}% ${100 - radiusOne}% ${100 - radiusThree}% ${radiusThree}% / ${radiusFour}% ${radiusTwo}% ${100 - radiusTwo}% ${100 - radiusFour}%`
+
+    // escribimos el css del blob con los datos de los inputs
+    document.querySelector('.output').style.cssText =
+        `border-radius:${borderRadius};
+    height:${blobHeight}px;
+    width:${blobWidth}px;`;
+
+    // imprimimos en pantalla el valor del border-radius
+    document.querySelector('.output-code').innerHTML = `${borderRadius}`;
+
+}
+
+function copy() {
+    var r = document.createRange();
+    r.selectNode(outputcode);
+    window.getSelection().removeAllRanges();
+    window.getSelection().addRange(r);
+    document.execCommand('copy');
+    window.getSelection().removeAllRanges();
+}
