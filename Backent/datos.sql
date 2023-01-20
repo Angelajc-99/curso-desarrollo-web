5e1cd0db4a84c73c30fb641182088f1a1fbe7219
Angelajc-99 <nicolejara053@gmail.com>
2023-01-20 15:02:25 +0100

    clase del viernes 20 de enero

--- Backent/datos.sql
+++ Backent/datos.sql
@@ -10,7 +10,37 @@ CREATE TABLE espectaculos (
 
 CREATE TABLE precios_espectaculos (
     cod_espectaculo VARCHAR(8) NOT NULL,
-    cod_recinto VARCHAR(80) NOT NULL,
+    cod_recinto VARCHAR(8) NOT NULL,
     zona VARCHAR(80) NOT NULL,
     precio DECIMAL NOT NULL
 );
+
+CREATE TABLE recintos (
+    cod_recinto VARCHAR(8) NOT NULL,
+    nombre VARCHAR(80) NOT NULL,
+    direccion VARCHAR(80) NOT NULL,
+    ciudad VARCHAR(80) NOT NULL,
+    telefono VARCHAR(80),
+    horario VARCHAR(80) NOT NULL
+);
+
+CREATE TABLE zonas_recintos (
+    cod_recinto VARCHAR(8) NOT NULL,
+    zona VARCHAR(80) NOT NULL,
+    capacidad INTEGER NOT NULL
+);
+
+CREATE TABLE asientos (
+    cod_recinto VARCHAR(8) NOT NULL,
+    zona VARCHAR(80) NOT NULL,
+    fila INTEGER NOT NULL,
+    numero INTEGER NOT NULL
+);
+
+CREATE TABLE representaciones (
+    cod_espectaculo varchar(8) NOT NULL,
+    fecha 
+    hora DATE
+
+
+)
